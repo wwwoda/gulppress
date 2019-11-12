c266809ef8cc80ce3fa69866ad7fe0aa04a11615
David Mondok <david@woda.at>
2019-11-12 09:26:40 +0100

    updating package.jsons ...

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,37 @@
 module.exports = {
-	'env': {
-		'browser': true,
-		'commonjs': true,
-		'es6': true,
-		'node': true,
+	env: {
+		browser: true,
+		commonjs: true,
+		es6: true,
+		node: true,
 	},
-	'parser': '@typescript-eslint/parser',
-	'parserOptions': {
-		'project': './tsconfig.json',
-	},
-	'plugins': [
-    '@typescript-eslint',
-	],
-	'extends': [
+	extends: [
     'airbnb-typescript/base',
     "plugin:@typescript-eslint/recommended",
 	],
-	'rules': {
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: './tsconfig.json',
+		tsconfigRootDir: __dirname,
+	},
+	plugins: [
+    '@typescript-eslint',
+	],
+	rules: {
     'no-console': 'off',
     'no-restricted-syntax': 'off',
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/no-namespace': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
+	},
+	settings: {
+		'import/parsers': {
+			'@typescript-eslint/parser': ['.ts', '.tsx'],
+		},
+		'import/resolver': {
+			typescript: {
+				directory: __dirname,
+			},
+		},
 	},
 };
