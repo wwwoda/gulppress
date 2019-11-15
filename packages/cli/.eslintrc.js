2166f1d1ba0e78291f5dec1fe54531880e1de302
David Mondok <david@woda.at>
2019-11-15 21:15:04 +0100

    ..

--- packages/cli/.eslintrc.js
+++ packages/cli/.eslintrc.js
@@ -1,16 +1,42 @@
 module.exports = {
+	env: {
+		browser: true,
+		commonjs: true,
+		es6: true,
+		node: true,
+	},
+	parser: '@typescript-eslint/parser',
+	extends: [
+		'airbnb-typescript/base',
+	],
+	plugins: [
+		'@typescript-eslint',
+	],
 	parserOptions: {
 		project: './tsconfig.json',
 		tsconfigRootDir: __dirname,
-	},
-	settings: {
-		'import/resolver': {
-			typescript: {
-				directory: __dirname,
-			},
+		ecmaVersion: 2018,
+		ecmaFeatures: {
+			jsx: true,
 		},
-	},
+		sourceType: 'module'
+  },
+  "settings": {
+    "import/resolver": {
+      "node": {
+        "extensions": [".js", ".jsx", ".ts", ".tsx"]
+      }
+    }
+  },
+	// settings: {
+	// 	'import/resolver': {
+	// 		typescript: {
+	// 			directory: __dirname,
+	// 		},
+	// 	},
+	// },
 	rules: {
 		'no-console': 'off',
-	},
+    'no-restricted-syntax': 'off'
+  }
 };
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,4 +1,4 @@
-// import chalk from 'chalk';
+import chalk from 'chalk';
 // import run from './modules/run';
 
 const currentNodeVersion = process.versions.node;
@@ -9,8 +9,8 @@ const major = parseInt(semver[0], 10);
 if (major < 8) {
   // eslint-disable-next-line  no-console
   console.error(
-    // chalk.red(`GulpPress requires Node 8 or higher while you are running Node ${currentNodeVersion}\n
-      Please update your version of Node.`),
+    chalk.red(`GulpPress requires Node 8 or higher while you are running Node ${currentNodeVersion}\n
+    //   Please update your version of Node.`),
   );
   process.exit(1);
 }
