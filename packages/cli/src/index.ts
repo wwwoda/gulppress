9ea190c833b71a34ff7cf02eeccf75b70878b870
David Mondok <david@woda.at>
2019-11-15 20:59:51 +0100

    ..

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,5 +1,5 @@
-import chalk from 'chalk';
-import run from './modules/run';
+// import chalk from 'chalk';
+// import run from './modules/run';
 
 const currentNodeVersion = process.versions.node;
 const semver = currentNodeVersion.split('.');
@@ -9,7 +9,7 @@ const major = parseInt(semver[0], 10);
 if (major < 8) {
   // eslint-disable-next-line  no-console
   console.error(
-    chalk.red(`GulpPress requires Node 8 or higher while you are running Node ${currentNodeVersion}\n
+    // chalk.red(`GulpPress requires Node 8 or higher while you are running Node ${currentNodeVersion}\n
       Please update your version of Node.`),
   );
   process.exit(1);
