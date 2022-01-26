ec6ebb11a86481bf17cab99b99a44352210bdd6d
David Mondok <3883758+davidmondok@users.noreply.github.com>
2022-01-26 19:01:57 +0100

    add new test assets

--- packages/test/gulpfile.ts
+++ packages/test/gulpfile.ts
@@ -5,36 +5,45 @@ import getImagesTask, { getClearImagesCacheTask } from '@gulppress/images';
 import getTranslationTask from '@gulppress/translate';
 
 task('favicon', getFaviconTask({
-  src: './assets/images/1000.svg',
+  src: './assets/images/favicon.svg',
   dest: './build/favicon',
   manifest: {
     name: 'Test',
     short_name: 'Test',
-    background_color: '#ffffff',
-    theme_color: '#ffffff',
+    background_color: '#fcb900',
+    theme_color: '#fcb900',
     display: 'browser',
   },
 }));
 
-// task('fonts', getFontsTask({
-//   src: './assets/fonts/**/*',
-//   dest: './build/fonts',
-// }));
-
 task('fonts', getFontsTask({
-  src: './assets/fonts/**/*.ttf',
+  src: './assets/fonts/**/*',
   dest: './build/fonts',
   fontFactoryConfigs: {
     '*.ttf': [{
-      format: ['woff', 'woff2'],
+      format: ['woff', 'woff2', 'ttf'],
       subsetUnicodeBlockRanges: ['Latin Alphabet', 'Digits', 'German', 'Punctuation & Symbols Minimal'],
       rename: {
         suffix: '-subset',
       },
     }, {
-      format: ['woff', 'woff2'],
+      format: ['woff', 'woff2', 'ttf'],
       subsetUnicodeBlockRanges: ['Basic Latin', 'Latin-1 Supplement', 'German'],
     }],
+    '*.woff': {
+      format: ['woff', 'woff2', 'ttf'],
+      subsetUnicodeBlockRanges: ['Basic Latin'],
+      rename: {
+        suffix: '-from-woff',
+      },
+    },
+    '*.woff2': {
+      format: ['woff', 'woff2', 'ttf'],
+      subsetText: 'ABCabc',
+      rename: {
+        suffix: '-from-woff2',
+      },
+    },
   },
   fontFactoryOptions: {
     passThroughMatched: false,
@@ -46,22 +55,18 @@ task('images', getImagesTask({
   src: './assets/images/**/*',
   dest: './build/images',
   destPhpPartials: './build/partials/images',
-}));
-
-task('images', getImagesTask({
-  src: './assets/images/**/*',
-  dest: './build/images',
-  destPhpPartials: './build/partials/images',
   imageFactoryConfigs: {
-    'austria*.png': [
+    '*.jpeg': [
       { resize: { width: 100 }, format: ['webp', 'jpeg', 'png'], rename: { suffix: '-100' } },
       { resize: { width: 150 }, format: ['webp', 'jpeg', 'png'], rename: { suffix: '-150' } },
       { resize: { width: 200 }, format: ['webp', 'jpeg', 'png'], rename: { suffix: '-200' } },
     ],
-    'foo*.png': {
+    '*.png': {
+      format: ['jpeg', 'png', 'webp', 'avif'],
       resize: {
         width: 100,
       },
+      rename: { suffix: '-100' },
     },
   },
 }));
@@ -74,7 +79,7 @@ task('icons', getImagesTask({
 }));
 
 task('translate', getTranslationTask({
-  src: 'php/**/*.php',
+  src: './assets/php/**/*.php',
   dest: 'build/languages/test.pot',
   // Available gulp-wp-pot options: https://github.com/wp-pot/wp-pot#options
   wpPotOptions: {
